2abe876b811fca14c12742f8b5df2e9b0febb8ca
ilanuk_windows <ilanuk@yahoo.com>
2017-08-03 22:42:13 -0400

    Fix the bugs in the coding 2

--- assets/main.js
+++ assets/main.js
@@ -19,7 +19,7 @@ function guess() {
         showAnswer(true);
         showReplay();
     }
-    else if ((!getResults(input.value)) && (attempt.value==10)){
+    else if ((getResults(input.value)==false) && (attempt.value>=10)){
         setMessage("You Lose! :(");
         showAnswer(false);
         showReplay();
